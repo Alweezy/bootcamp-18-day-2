0fc06636bb1be8273d3f885c7f46044c773e3461
Alweezy <alvinmutisya@gmail.com>
2017-05-09 13:00:45 +0300

    Initial commit

---
+++ oop_concepts/sports_man_tests.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from sports_man import SportsMan
+
+
+class TestCasesSportsMan(TestCase):
+    """Tests the functionality of the base class SportsMan"""
+    def SetUp(self):
+        """Grants access to all the testcases an instance of SportsMan"""
+        self.sportsman = SportsMan()
+
+    def tearDown(self):
+        del self.sportsman
+
+    def test_sportsman_full_name(self):
+        self.assertTrue(self.sportsman.fullname('John' + 'Doe'), 'John Doe')
+
+    def test_pay_rise_is_right(self):
+        self.pay_raise_index = 1.5
+        self.assertTrue(self.sportsman.pay_rise(2000 * self.pay_raise_index), 30000)
