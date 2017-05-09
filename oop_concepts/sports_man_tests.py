9f069b6620369a36159e1322c46e727edc6534e8
Alweezy <alvinmutisya@gmail.com>
2017-05-09 14:20:19 +0300

    Restructure tests

--- oop_concepts/sports_man_tests.py
+++ oop_concepts/sports_man_tests.py
@@ -4,16 +4,11 @@ from sports_man import SportsMan
 
 class TestCasesSportsMan(TestCase):
     """Tests the functionality of the base class SportsMan"""
-    def SetUp(self):
-        """Grants access to all the testcases an instance of SportsMan"""
-        self.sportsman = SportsMan()
-
-    def tearDown(self):
-        del self.sportsman
 
     def test_sportsman_full_name(self):
-        self.assertTrue(self.sportsman.fullname('John' + 'Doe'), 'John Doe')
+        self.sportsman = SportsMan('Allan', 'Smith', 60000)
+        self.assertTrue(self.sportsman.fullname(), 'Allan Smith')
 
     def test_pay_raise_is_right(self):
-        self.pay_raise_index = 1.5
-        self.assertTrue(self.sportsman.pay_raise(2000 * self.pay_raise_index), 30000)
+        self.sportsman = SportsMan('Allan', 'Smith', 60000)
+        self.assertEqual(self.sportsman.pay_raise(), 90000)
