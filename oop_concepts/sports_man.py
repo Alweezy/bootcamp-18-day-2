6ef3e1e4629d15b7902ca8de8fa5d301cb6ae1ea
Alweezy <alvinmutisya@gmail.com>
2017-05-09 14:37:30 +0300

    Implement the parent class

--- oop_concepts/sports_man.py
+++ oop_concepts/sports_man.py
@@ -1,2 +1,16 @@
 class SportsMan(object):
-    pass
+    pay_raise_index = 1.5
+
+    def __init__(self,  fname, lname, salary):
+        self.fname = fname
+        self.lname = lname
+        self.salary = salary
+
+    def fullname(self):
+        return self.fname + ' ' + self.lname
+
+    def pay_raise(self):
+        self.salary *= self.pay_raise_index
+        return self.salary
+
+    
