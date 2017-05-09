8074c07befc0b2a1231396a4429e3e4b9422d3a2
Alweezy <alvinmutisya@gmail.com>
2017-05-09 16:55:48 +0300

    Add comments

--- programming_logic/data_types.py
+++ programming_logic/data_types.py
@@ -1,10 +1,16 @@
 def data_type(value):
+    """A function called data_type, that takes one argument,
+    compares and returns results, based on the argument supplied to the function.
+    """
+    # check if it is a string, return the length of that string
     if isinstance(value, str):
         return len(value)
+    # check there is no value, return default string
     elif value is None:
         return 'no value'
     elif isinstance(value, bool):
         return value
+    # check if value is an integer, perform further evaluations
     elif isinstance(value, int):
         if value < 100:
             return 'less than 100'
@@ -14,6 +20,7 @@ def data_type(value):
             return 'more than 100'
         else:
             return 'integer value not recognized'
+    # check if value is a list, determine the size of list.
     elif isinstance(value, list):
         if len(value) >= 3:
             return value[2]
